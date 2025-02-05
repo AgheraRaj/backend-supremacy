7d9a720512b8387affbb6ceaa28be60d91eb3378
AgheraRaj <rajaghera2004@gmail.com>
2025-02-05 16:44:43 +0530

    string

--- redis/string.js
+++ redis/string.js
@@ -3,6 +3,9 @@ const client = require("./client");
 async function init() {
   const result = await client.set("subject:1" , "maths")
   console.log(result);
+
+  const getResult = await client.get("subject:1")
+  console.log(getResult);
 };
 
 init();
